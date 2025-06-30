0674637c4abf846319bb24c1d3e44535a5e4d61a
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-06-30 09:53:14 +0200

    service worker

--- pwa-front-auth-test-app/app/page.tsx
+++ pwa-front-auth-test-app/app/page.tsx
@@ -1,9 +1,13 @@
-
+import Head from "next/head";
+import SWRegister from "@/components/clientComponents/SWRegister";
 
 export default function Home() {
   return (
     <div className="">
-
+      <Head>
+        <link rel="icon" href="/apple-touch-icon.png"/>
+      </Head>
+      <SWRegister/>
     </div>
   );
 }
